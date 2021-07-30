1a5be980c80f27ad1a98720aa0330005a884fcc0
Faisal <saleemfs27@gmail.com>
2021-07-30 22:10:37 +0200

    Added ERC-20 Token code

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -5,17 +5,28 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20 {
   //add minter variable
+  address public minter;
 
   //add minter changed event
+  event MinterChanged(address indexed from, address to);
 
-  constructor() public payable ERC20("Name", "Symbol") {
+  constructor() public payable ERC20("Lethereum", "LETH") {
     //asign initial minter
+    minter = msg.sender;
   }
 
   //Add pass minter role function
+  function passMinterRole(address dBank) public returns (bool) {
+    require(msg.sender == minter, 'Error, only current minter can pass minter role.');
+    minter = dBank;
+
+    emit MinterChanged(msg.sender, dBank);
+    return true;
+  }
 
   function mint(address account, uint256 amount) public {
     //check if msg.sender have minter role
+    require(msg.sender == minter, 'Error, sender does not have a minter role.');
 		_mint(account, amount);
 	}
 }
